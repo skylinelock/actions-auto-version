ac6fa1c11b32a5fe8caa7dcef9f7e6fd704f5a4d
sky_lock <sho59sky@outlook.jp>
2021-03-26 15:33:17 +0900

    Add: tag flow

--- scripts/count_version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "count_version.sh"
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
-chmod u+x ./count_version.sh
 
-echo "release.sh"
+echo "Running release.sh"
+
+pre_version=`git tag -l | tail -n 1 | awk '{print $1}'`
+
+prefix=`echo $PR_TITLE | awk '{print $1}'`
+
+command=${prefix,,}
+
+if [ -z "$pre_version" ]; then
+    echo "1.0.0" 
+    exit 1
+fi
+
+fix=`echo $pre_version | awk -F '.' '{print $3}'`
+minor=`echo $pre_version | awk -F '.' '{print $2}'`
+major=`echo $pre_version | awk -F '.' '{print $1}'`
+
+case "$command" in
+    "fix" ) fix=`expr $fix + 1` ;;
+    "minor" ) 
+        fix=0
+        minor=`expr $minor + 1`
+        ;;
+    "major" ) 
+        fix=0
+        minor=0
+        major=`expr $major + 1`
+        ;;
+    * ) fix=`expr $fix + 1` ;;
+esac
+
+version="v$major.$minor.$fix"
+git tag -a $version
+git push origin $version
